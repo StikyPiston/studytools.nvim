85d8170cdd738d20a251861abf6827a5e43563cd
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-24 22:26:00 +0200

    feat: StudytoolsPomodoroStatus command

--- lua/studytools/pomodoro.lua
+++ lua/studytools/pomodoro.lua
@@ -1,7 +1,8 @@
 local M = {}
 
-local timer = nil
-local state = nil
+local timer        = nil
+local state        = nil
+local phaseEndTime = nil
 
 local ringBell = function()
 	vim.api.nvim_echo({ { "\a", "" } }, false, {})
@@ -22,6 +23,9 @@ end
 
 startPhase = function(durationMinutes, nextState, nextDuration)
 	stopTimer()
+
+	local durationSeconds = durationMinutes * 60
+	phaseEndTime = os.time() + durationSeconds
 
 	timer = vim.loop.new_timer()
 	timer:start(
@@ -64,6 +68,28 @@ M.stop = function()
 	notify("Pomodoro stopped")
 end
 
+M.status = function()
+	if not timer or not state or not phaseEndTime then
+		notify("No active Pomodoro session")
+		return
+	end
+
+	local secondsLeft = phaseEndTime - os.time()
+	if secondsLeft < 0 then secondsLeft = 0 end
+
+	local minutes = math.floor(secondsLeft / 60)
+	local seconds = secondsLeft % 60
+
+	local msg = string.format(
+		"Current session: %s | Time remaining: %02d:%02d",
+		state == "work" and "Work" or "Break",
+		minutes,
+		seconds
+	)
+
+	notify(msg)
+end
+
 M.setup = function()
 	vim.api.nvim_create_user_command(
 		"StudytoolsPomodoro",
@@ -76,6 +102,12 @@ M.setup = function()
 	vim.api.nvim_create_user_command(
 		"StudytoolsPomodoroStop",
 		M.stop,
+		{}
+	)
+	
+	vim.api.nvim_create_user_command(
+		"StudytoolsPomodoroStatus",
+		M.status,
 		{}
 	)
 end
